1216df06eaa215c22a0571d3109ab8345ec2c95b
Estebangmz666 <estebangumy05@gmail.com>
2024-04-27 13:50:22 -0500

    Commit inicial del proyecto

---
+++ src/Login/UserLoginController.java
@@ -0,0 +1,23 @@
+package Login;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class UserLoginController {
+
+    @FXML
+    void btnIniciarSesionClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistrarseClicked(ActionEvent event) {
+
+    }
+     
+    @FXML
+    void btnCerrarProgamaClicked(ActionEvent event) {
+        System.exit(0);
+    }
+
+
+}
